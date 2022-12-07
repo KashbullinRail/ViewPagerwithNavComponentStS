46f1dbee28db7f9bc444379fdba075d362d38111
KashbullinRail <kashbullinrail@gmail.com>
2022-12-07 20:09:19 +0300

    finish xml FirstScreen.kt, SecondScreen.kt, ThirdScreen.kt

--- app/src/main/java/com/example/viewpagerwithnavcomponentsts/onboarding/ViewPagerFragment.kt
+++ app/src/main/java/com/example/viewpagerwithnavcomponentsts/onboarding/ViewPagerFragment.kt
@@ -6,6 +6,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.viewpagerwithnavcomponentsts.R
+import com.example.viewpagerwithnavcomponentsts.onboarding.screens.FirstScreen
+import com.example.viewpagerwithnavcomponentsts.onboarding.screens.SecondScreen
+import com.example.viewpagerwithnavcomponentsts.onboarding.screens.ThirdScreen
 
 
 class ViewPagerFragment : Fragment() {
@@ -15,7 +18,15 @@ class ViewPagerFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_view_pager, container, false)
+        val view = inflater.inflate(R.layout.fragment_view_pager, container, false)
+
+        val fragmentList = arrayListOf<Fragment>(
+            FirstScreen(),
+            SecondScreen(),
+            ThirdScreen()
+        )
+
+        return view
     }
 
 
