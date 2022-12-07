19542ed7a93d17b8f2f30cbf873c44cd63232d6a
KashbullinRail <kashbullinrail@gmail.com>
2022-12-07 20:32:31 +0300

    create ViewPagerAdapter

--- app/src/main/java/com/example/viewpagerwithnavcomponentsts/onboarding/ViewPagerFragment.kt
+++ app/src/main/java/com/example/viewpagerwithnavcomponentsts/onboarding/ViewPagerFragment.kt
@@ -5,14 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.viewpager.widget.ViewPager
 import com.example.viewpagerwithnavcomponentsts.R
 import com.example.viewpagerwithnavcomponentsts.onboarding.screens.FirstScreen
 import com.example.viewpagerwithnavcomponentsts.onboarding.screens.SecondScreen
 import com.example.viewpagerwithnavcomponentsts.onboarding.screens.ThirdScreen
 
 
-class ViewPagerFragment : Fragment() {
+class ViewPagerFragment : Fragment(R.layout.fragment_view_pager) {
 
+//    private val viewPager:ViewPager by lazy { requireActivity().findViewById(R.id.viewPager) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,8 +28,18 @@ class ViewPagerFragment : Fragment() {
             ThirdScreen()
         )
 
+        val adapter = ViewPagerAdapter(
+            fragmentList,
+            requireActivity().supportFragmentManager,
+            lifecycle
+        )
+
+            view.findViewById<ViewPager>(R.id.viewPager).adapter = adapter
+
         return view
     }
+
+
 
 
 }
